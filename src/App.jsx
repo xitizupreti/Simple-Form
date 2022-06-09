4e825121a35b0fcb86e4a24062bfd3b1c9a0d0d9
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-06-09 19:14:52 +0545

    intro to hooks // old state

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
-ttest
+import React,{useState} from 'react';
+
+const App = () =>{
+    const state= useState();
+const [count /*current value*/ ,setCount /*Updated value*/] = useState(0)/*initial value*/; 
+
+const Inc=()=>{
+setCount(count+1);
+}
+
+return(
+    <>
+    <div>
+    <h1>{count}</h1>
+    <button onClick={Inc} >Click here</button>
+    </div>
+    </>
+)
+}
+
+export default App;
