2278f08c84e12db8a847f4e0b42c649c7e779a6c
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-06-21 23:21:35 +0545

    spread operator easy form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,110 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [state, setState]=useState();
-  const [state1, setState1]=useState();
-  const [Full,setFull]=useState();
-  const [Last,setLast]=useState();
-  const e=(dis)=>{
-    var val=setState(dis.target.value);
-    return val;
-  }
-  const e1=(dis)=>{
-    var val1=setState1(dis.target.value);
-    return val1;
-  }
-  const done=(event)=>{
+  const [fullname, setfullname] = useState({
+    fname: "",
+    lname: "",
+    email:"",
+    phone:"",
+  });
+
+  const e = (event) => {
+
+    const{value,name}= event.target;
+
+    setfullname((preValue) => {
+      return{
+        ...preValue,
+        [name]: value,
+      }
+      // if (name === "fname") {
+      //   return {
+      //     fname: value,
+      //     lname: preValue.lname,
+      //     email: preValue.email,
+      //     phone: preValue.phone,
+      //   };
+      // }
+      // else if (name === "lname") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: value,
+      //     email: preValue.email,
+      //     phone: preValue.phone,
+      //   };
+      // }
+      // else if (name === "email") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     email: value,
+      //     phone: preValue.phone,
+      //   };
+      // }
+      // else if (name === "phone") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     email: preValue.email,
+      //     phone: value,
+      //   };
+      // }
+    });
+  };
+
+  const done = (event) => {
     event.preventDefault();
-    setFull(state);
-    setLast(state1);
-  }
+    alert("submitted");
+  };
+
   return (
     <>
-    <form onSubmit={done}>
-      <div>
-        <h1>Hello {Full} {Last}</h1>
-        <input type="text" placeholder="Enter your name" onChange={e} value={state}></input>
-        <input type="text" placeholder="Enter your pw" onChange={e1} value={state1}></input>
-        <button>Click Me ❤️</button>
-      </div>
+      <form onSubmit={done}>
+        <div>
+          <h1>
+            Hello {fullname.fname} {fullname.lname}
+          </h1>
+          <p>{fullname.email}</p>
+          <p>{fullname.phone}</p>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="fname"
+            onChange={e}
+            value={fullname.fname}
+            autoComplete="off"
+          ></input>
+          <input
+            type="text"
+            placeholder="Enter your pw"
+            name="lname"
+            onChange={e}
+            value={fullname.lname}
+            autoComplete="off"
+          ></input>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            onChange={e}
+            value={fullname.email}
+            autoComplete="off"
+          ></input>
+          <input
+            type="number"
+            placeholder="Enter your pno"
+            name="phone"
+            onChange={e}
+            value={fullname.phone}
+            autoComplete="off"
+          ></input>
+          <button>Click Me ❤️</button>
+        </div>
       </form>
     </>
   );
 };
+
+// ... => spread operator to call whole array
 
 export default App;
